a7f31d10adf4daac306cad2ebc8991cda66d87ab
JoelTokatli <jamescarter6667@gmail.com>
2022-10-06 18:51:32 +0200

    added html and styling

---
+++ index.js
@@ -0,0 +1,22 @@
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+let url =
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_Kf7MTroFGT2dZyEZAvt0bnRIhXSF3&ipAddress=";
+
+const fetchData = async () => {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
