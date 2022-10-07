075059132664c1c7b990bf8d6583796896ebb706
JoelTokatli <jamescarter6667@gmail.com>
2022-10-07 19:07:32 +0200

    changing visual styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    safelist: ["bg-hero"],
-    theme: {
-      extend: {
-        backgroundImage: {
-          hero: "url('images/pattern-bg.png')",
-        },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: "bg-hero",
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('images/pattern-bg.png')",
       },
     },
   },
