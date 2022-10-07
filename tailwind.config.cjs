915199705866ae7690807aac13318787fa8d3dae
JoelTokatli <jamescarter6667@gmail.com>
2022-10-07 23:02:03 +0200

    uploading tab icon and changing font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,8 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   safelist: "bg-hero",
   theme: {
-    extend: {
-      backgroundImage: {
-        hero: "url('images/pattern-bg.png')",
-      },
+    fontFamily: {
+      rubik: ["Rubik", "sans-serif"],
     },
   },
   plugins: [],
