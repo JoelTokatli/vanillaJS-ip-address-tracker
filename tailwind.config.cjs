bd940a3344060f8e73499f3bc4fa8ae72794e938
JoelTokatli <jamescarter6667@gmail.com>
2022-10-07 19:32:14 +0200

    removing unnecesary files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   safelist: "bg-hero",
   theme: {
     extend: {
